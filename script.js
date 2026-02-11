200a7145385547b3c1b3cfd7129dfb471129bbfa
Alisher <adododzhonov@inbox.ru>
2026-02-11 07:08:26 +0300

    Fix formatting and simplify condition

--- script.js
+++ script.js
@@ -1,55 +1,44 @@
+// Покраска первой карточки
+const recolorBackgroundFirstCardButton = document.getElementById('recolor-background-first-card-button');
+const firstProductCard = document.querySelector('.product-card');
+const blueHashColor = '#0000ff';
 
-//const a = document.querySelector('#recolor-background-first-card');
-
-
-
-//Покраска первой карточки
-
-const recolorBackgroundFirstCardButton = document.getElementById('recolor-background-first-card-button')
-const firstProductCard = document.querySelector('.product-card')
-const blueHashColor = '#0000ff'
-
-recolorBackgroundFirstCardButton.addEventListener('click', ()=> {
-    firstProductCard.style.backgroundColor = blueHashColor;
-})
+recolorBackgroundFirstCardButton.addEventListener('click', () => {
+  firstProductCard.style.backgroundColor = blueHashColor;
+});
 
 // Покраска всех карточек
-
-const recolorBackgroundAllCardsButton = document.getElementById('recolor-background-all-cards-button')
-const allProductCards = document.querySelectorAll('.product-card')
+const recolorBackgroundAllCardsButton = document.getElementById('recolor-background-all-cards-button');
+const allProductCards = document.querySelectorAll('.product-card');
 const greenHashColor = '#00ff44';
 
+recolorBackgroundAllCardsButton.addEventListener('click', () => {
+  allProductCards.forEach((card) => {
+    card.style.backgroundColor = greenHashColor;
+  });
+});
 
-recolorBackgroundAllCardsButton.addEventListener('click', ()=> {
-    allProductCards.forEach (
-        card => card.style.backgroundColor = greenHashColor)
-})
-
-//Переход на страницу Google
-
-const openGooglebutton = document.getElementById('open-google-button')
+// Переход на страницу Google
+const openGooglebutton = document.getElementById('open-google-button');
 openGooglebutton.addEventListener('click', openGoogle);
 
-function openGoogle () {
-const answer = confirm ('Вы действительно хотите перейти на сайт Google?')   
+function openGoogle() {
+  const answer = confirm('Вы действительно хотите перейти на сайт Google?');
 
-if (answer === true) {
-    window.open ('https://google.com');
-} else {
-
-}console.log('Пользователь отменил переход на Google')
-}
+  if (answer) {
+    window.open('https://google.com');
+  } else {
+    console.log('Пользователь отменил переход на Google');
+  }};
 
 // Задание 3: вывод контента заголовка при наведении
-
 const catalogTitle = document.querySelector('.catalog__title');
 
-catalogTitle.addEventListener('mouseover', function () {
+catalogTitle.addEventListener('mouseover', () => {
   console.log(catalogTitle.textContent);
 });
 
 // Задание 4: кнопка меняет свой цвет (toggle)
-
 const toggleButton = document.getElementById('toggle-button-color');
 
 toggleButton.addEventListener('click', () => {
